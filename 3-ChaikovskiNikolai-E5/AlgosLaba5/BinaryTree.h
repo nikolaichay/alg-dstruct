a1d9f78677af5afb4d009a0d84f0fec2383122c1
Nikolai <nikolaichay@mail.ru>
2021-12-27 08:21:13 +0300

    Update BinaryTree.h

--- 3-ChaikovskiNikolai-E5/AlgosLaba5/BinaryTree.h
+++ 3-ChaikovskiNikolai-E5/AlgosLaba5/BinaryTree.h
@@ -5,7 +5,7 @@ typedef struct tree_t {
 	struct tree_t* left, * right;
 }tree_t;
 int TotalDigit(int val);
-void FillLen(tree_t** tree);
+void FillLen(tree_t* tree);
 tree_t* Insert(tree_t* tree, int val);
 void FreeTree(tree_t* tree);
-int PrintTree(tree_t* tree, int n,FILE* in);
+int PrintTree(tree_t* tree, int n, FILE* in);
